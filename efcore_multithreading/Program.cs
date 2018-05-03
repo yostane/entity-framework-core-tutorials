65ad5834426c915c005acde79c0a3f3bc8add1c8
yostane <yostane@yahoo.fr>
2018-05-03 22:47:08 +0200

    Multithreading wip

---
+++ efcore_multithreading/Program.cs
@@ -0,0 +1,43 @@
+﻿﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace efcore_multithreading {
+    class Program {
+        static void Main (string[] args) {
+            Console.WriteLine ("Entity Framework Core with multithreading!");
+            Setup ();
+
+            Console.WriteLine ("Addng games");
+
+            Parallel.For (0, 30, i => {
+                using (var context = new VideoGamesDatabaseContext ()) {
+                    context.VideoGames.Add (new VideoGame {
+                        Title = $"Game {i}",
+                            Platform = "multi",
+                            ReleaseYear = 2017
+                    });
+                    context.SaveChanges ();
+                }
+            });
+
+            Console.WriteLine ("Games");
+            using (var context = new VideoGamesDatabaseContext ()) {
+                foreach (var videoGame in context.VideoGames) {
+                    Console.WriteLine ($"Title: {videoGame.Title}");
+                }
+            }
+        }
+
+        static void Setup () {
+            using (var context = new VideoGamesDatabaseContext ()) {
+
+                context.Database.EnsureDeleted ();
+                context.Database.EnsureCreated ();
+            }
+        }
+    }
+}
